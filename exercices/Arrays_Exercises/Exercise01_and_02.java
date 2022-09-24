dfd4cdac2e4ed7c86e48fa9f6dd90f3e40e2c8a8
Rafael <rafaelrodriguesmacedo2004@gmail.com>
2022-09-23 23:02:03 -0300

    ex03 added

--- exercices/Arrays_Exercises/Exercise01_and_02.java
+++ exercices/Arrays_Exercises/Exercise01_and_02.java
@@ -7,16 +7,17 @@ import java.util.Scanner;
 
 public class Exercise01_and_02 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int[] Array = new int[10];
+        try (Scanner input = new Scanner(System.in)) {
+            int[] Array = new int[10];
 
-        for(int i = 1; i <= Array.length; i++){
-            System.out.printf("Type a number to the array at %d position: ", i);
-            Array[i] = input.nextInt();
-        }
+            for(int i = 1; i <= Array.length; i++){
+                System.out.printf("Type a number to the array at %d position: ", i);
+                Array[i] = input.nextInt();
+            }
 
-        for(int i = 1; i <= Array.length;i++){
-            System.out.printf("The %d position in the array value is: %d \n", i, Array[i]);
+            for(int i = 1; i <= Array.length;i++){
+                System.out.printf("The %d position in the array value is: %d \n", i, Array[i]);
+            }
         }
 
     }
--- exercices/Arrays_Exercises/Exercise03.java
+++ exercices/Arrays_Exercises/Exercise03.java
@@ -1,5 +1,46 @@
+/*
+    Escreva um programa que receba a nota de 10 alunos.
+    Sabendo que os alunos com nota inferior a 5 estão reprovados, os alunos com nota
+    superior a 6 estão aprovados Liste o total de alunos aprovados , o total de alunos em
+    recuperação e os reprovados.
+    Imprima as notas
+ */
+
+import java.util.Scanner;
+
 public class Exercise03 {
     public static void main(String[] args) {
-        
+        try (Scanner input = new Scanner(System.in)) {
+            int[] grades = new int[10]; //array w/ 10 positions
+            int reprovedStudents = 0;
+            int recuperationStudents = 0;
+            int approvedStudents = 0;
+
+            for(int i = 0; i < 10; i++){
+                System.out.printf("Type a grade for the student number %d: \n", i);
+                grades[i] = input.nextInt();
+            }
+
+            for(int i = 0; i < 10; i++){
+                if(grades[i] < 5){
+                    reprovedStudents++;
+                } else if (grades[i] > 4 && grades[i] < 7){
+                    recuperationStudents++;
+                } else {
+                    approvedStudents++;
+                }
+            }
+
+            System.out.println("Students grades: \n");
+
+            for(int i = 0; i < 10; i++){
+                System.out.printf("Student number %d: %d \n", i, grades[i]);
+            }
+            
+                System.out.printf("Number of reproved students was: %d \n", reprovedStudents);
+                System.out.printf("Number of recuperation students was: %d \n", recuperationStudents);
+                System.out.printf("Number of approved students was: %d \n", approvedStudents);
+        }
+            
     }
 }
