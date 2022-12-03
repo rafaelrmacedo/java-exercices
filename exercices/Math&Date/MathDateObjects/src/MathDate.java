26e6daa6b9461901091d005fe42d6bea82f02036
rafaelrmacedo <rafaelrodriguesmacedo2004@gmail.com>
2022-12-03 02:26:59 -0300

    some exercises

---
+++ exercices/Math&Date/MathDateObjects/src/MathDate.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class MathDate {
+
+    public void GetCurrentDate() {
+        Date today = new Date();
+
+        System.out.println("Today's date: " + today);
+    }
+    
+    public void SquareRoot(int number) {
+        double result = Math.sqrt(number);
+
+        System.out.println("");
+    }
+}
